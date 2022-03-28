edfe7ecfa7bc6cfe572621a7b7dfb8da73bf72f1
Jacek Leszczynski <sam@bialan.pl>
2022-03-28 08:53:12 +0200

    Rozbudowa klas - a właściwie stworzenie klasy podstawowej elementu TeElement, która będzie bazą wszystkich elementów mniej i bardziej rozbudowanych

--- unitelementy.pas
+++ unitelementy.pas
@@ -31,14 +31,36 @@ type
     property Count: integer read FCount;
   end;
 
+  { TeMap }
+
+  TeMap = class
+    private
+      tab: TStringList;
+      function GetCount: integer;
+    public
+      constructor Create;
+      destructor Destroy; override;
+      function Add(aRodzaj,aPiny: string): integer; //rodzaj: Z (zasilanie,cewki) P (piny: zaczynamy od domyślnych, w środku wspólny)
+      procedure Delete(aIndex: integer);
+      procedure Clear;
+      function GetPin(aState: boolean; aNrPin: integer): integer;
+    published
+      property Count: integer read GetCount;
+  end;
+
   { TeElement }
 
-  (* To jest moja klasa główna elementów,                    *)
-  (* wszystkie elementy będą budowane na tej właśnie klasie. *)
+  (* To jest moja klasa główna elementów,                               *)
+  (* wszystkie elementy będą budowane na tej właśnie klasie.            *)
+  (* Jeśli coś mam posłać dalej, to:                                    *)
+  (* - z Map dostaję nr pinu z którego mam posłać wartość ładunku dalej *)
+  (* - z Pins dostaję jaka wartość ładunku ma zostać posłana            *)
+  (* - z Lines dostaję gdzie na zewnątrz mam posłać w/w informacje      *)
 
   TeElement = class
   private
     FID: integer;
+    FMap: TeMap;
     FPins: TePins;
     FState: boolean;
     procedure SetState(AValue: boolean);
@@ -48,8 +70,10 @@ type
     destructor Destroy; override;
   published
     property Identificator: integer read FID write FID;
-    property State: boolean read FState write SetState default false;
-    property Pins: TePins read FPins write FPins;
+    property State: boolean read FState write SetState default false; //stan zasilenia elementu
+    property Pins: TePins read FPins write FPins; //wartości ładunków na kolejnych pinach
+    property Map: TeMap read FMap write FMap; //mapa pinów (pary, sekcje)
+    //property Lines: TeLines read FLines write FLines; //mapa podłączeń zewnętrznych, tzw. linie
   end;
 
   { TeZasilanie }
@@ -71,6 +95,85 @@ type
   end;
 
 implementation
+
+uses
+  ecode;
+
+{ TeMap }
+
+function TeMap.GetCount: integer;
+begin
+  result:=tab.Count;
+end;
+
+constructor TeMap.Create;
+begin
+  tab:=TStringList.Create;
+end;
+
+destructor TeMap.Destroy;
+begin
+  tab.Free;
+  inherited Destroy;
+end;
+
+function TeMap.Add(aRodzaj, aPiny: string): integer;
+begin
+  result:=tab.Add(aRodzaj+','+aPiny);
+end;
+
+procedure TeMap.Delete(aIndex: integer);
+begin
+  tab.Delete(aIndex);
+end;
+
+procedure TeMap.Clear;
+begin
+  tab.Clear;
+end;
+
+function TeMap.GetPin(aState: boolean; aNrPin: integer): integer;
+var
+  i,r,l: integer;
+  pom,s,s1,s2,s3: string;
+  p1,p2,p3: integer;
+begin
+  r:=0;
+  for i:=0 to tab.Count-1 do
+  begin
+    l:=-1;
+    pom:=tab[i];
+    s:=GetLineToStr(pom,1,',');
+    s1:=GetLineToStr(pom,2,',');
+    s2:=GetLineToStr(pom,3,',');
+    s3:=GetLineToStr(pom,4,',');
+    if s1<>'' then inc(l);
+    if s2<>'' then inc(l);
+    if s3<>'' then inc(l);
+    if l>0 then p1:=StrToInt(s1) else p1:=-1;
+    if l>1 then p2:=StrToInt(s1) else p2:=-1;
+    if l>2 then p3:=StrToInt(s1) else p3:=-1;
+    if s='Z' then
+    begin
+      if aNrPin=p1 then r:=p2 else
+      if aNrPin=p2 then r:=p1;
+    end else
+    if s='P' then
+    begin
+      if aState then
+      begin
+        if aNrPin=p1 then r:=0 else
+        if aNrPin=p2 then r:=p3 else
+        if aNrPin=p3 then r:=p2;
+      end else begin
+        if aNrPin=p1 then r:=p2 else
+        if aNrPin=p2 then r:=p1 else r:=0;
+      end;
+    end;
+    if r>-1 then break;
+  end;
+  result:=r;
+end;
 
 { TePrzycisk }
 
@@ -144,12 +247,14 @@ begin
   FID:=aID;
   FState:=false;
   FPins:=TePins.Create;
+  FMap:=TeMap.Create;
   for i:=1 to aCountPins do FPins.Add(piZero);
 end;
 
 destructor TeElement.Destroy;
 begin
   FPins.Free;
+  FMap.Free;
   inherited Destroy;
 end;
 
